6904043b43f8c5e5bb1755038d2c05e997971241
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-09 22:04:32 +0200

    components imported

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from "./common/Container/index";
+import Section from "./common/Section/index";
+import Header from "./common/Header/index";
+import axios from "axios"
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header></Header>
+
+      <Section></Section>
+    </Container>
   );
 }
 
