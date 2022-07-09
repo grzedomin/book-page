190ca22020ce1a1c69fef93982c69f3e555b9ea4
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-09 22:03:33 +0200

    container component created and styled

---
+++ src/common/Container/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 100%;
+    max-width: 1500px;
+    margin: auto;
+    background-color: rgb(241, 241, 241);
+`;
