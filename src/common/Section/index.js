5cf7e0d0d1e9603473b03011af3ef62f79d61068
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-09 22:03:48 +0200

    section created and styled

---
+++ src/common/Section/index.js
@@ -0,0 +1,10 @@
+import BooksList from "../../features/BooksList";
+import { Wrapper } from "./styled";
+
+const Section = () => (
+    <Wrapper>
+        <BooksList />
+    </Wrapper>
+);
+
+export default Section;
---
+++ src/common/Section/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    padding: 40px;
+    margin-top: 100px;
+`;
