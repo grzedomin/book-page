592b6b3731161bfaacefd9dd36ed0bd497d8a690
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-09 22:04:13 +0200

    BookList component created and styled

---
+++ src/features/BooksList/styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 30px;
+`;
+
+export const Tile = styled.div`
+height: 400px;
+width: 280px;
+background: #ccc;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 10px;
+padding: 15px;
+
+
+`
+export const BookImg = styled.div`
+    height: 300px;
+    width: 200px;
+    background-color: crimson;
+`;
+
+export const Info = styled.p`
+
+`;
