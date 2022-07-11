4d12e505ac0a53d2f5be17b43c43d96774f40d67
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-11 15:57:07 +0200

    added border bottom and changed h1 color

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -6,6 +6,7 @@ export const Wrapper = styled.header`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    border-bottom: 1px solid rgba(175, 175, 175, 0.85);
 
     @media (max-width: 767px){
         display: flex;
@@ -16,6 +17,8 @@ export const Wrapper = styled.header`
 
 export const Title = styled.h1`
     font-size: 42px;
+    font-weight: 600;
+    color: #404040;
 `;
 
 
@@ -28,10 +31,18 @@ export const Button = styled.button`
     padding: 10px;
     font-size: 15px;
     border: 1px solid #bbb;
-    margin: 5px;
+    border: none;
+    margin-left: 15px;
+    border-radius: 5px;
+    -webkit-box-shadow: 0px 0px 2px 0px rgba(175, 175, 175, 0.85);
+    -moz-box-shadow: 0px 0px 2px 0px rgba(175, 175, 175, 0.85);
+    box-shadow: 0px 0px 2px 0px rgba(175, 175, 175, 0.85);
 
     &:hover {
-        background-color: green;
+        background-color: #5D6F68;
+        color: white;
+        cursor: pointer;
+        transition: .3s;
     }
 
     @media (max-width: 767px){
