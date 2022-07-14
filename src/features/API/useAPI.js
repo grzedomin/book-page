4a946e0e2805952dec3bdb6df9eeaa6a26b974aa
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-14 22:42:36 +0200

    maxResults changed from 10 to 40

--- src/features/API/useAPI.js
+++ src/features/API/useAPI.js
@@ -9,7 +9,7 @@ export const useFetchData = () => {
 
     const searchBook = async () => {
         try {
-            const response = await axios(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${APIKey}`);
+            const response = await axios(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${APIKey}&maxResults=40`);
             console.log(response.data.items)
             setBooks(response.data.items);
 
