e754c8072f512eb46f6058875ee136a1ff2b0ad8
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-15 19:27:46 +0200

    useEffect removed, books are downloaded onClick

--- src/features/Main/index.js
+++ src/features/Main/index.js
@@ -2,19 +2,13 @@ import BooksList from "./BooksList";
 import { Search } from "./Search";
 import { Wrapper } from "./styled";
 import { useFetchData } from "../API/useAPI";
-import { useEffect } from "react";
 
 const Section = () => {
 
     const { books, search, setSearch, searchBook } = useFetchData();
 
-    useEffect(() => {
-        searchBook();
-    }, []);
-
     return (
         <Wrapper>
-
             <Search
                 search={search}
                 setSearch={setSearch}
