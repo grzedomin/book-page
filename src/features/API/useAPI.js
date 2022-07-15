85cab47f13ec55c101aaf8844e996aa413ebab92
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-15 20:39:32 +0200

    state added

--- src/features/API/useAPI.js
+++ src/features/API/useAPI.js
@@ -5,17 +5,26 @@ import { APIKey } from "./APIKey";
 export const useFetchData = () => {
     const [search, setSearch] = useState("");
     const [books, setBooks] = useState([]);
-
+    const [fetchState, setFetchState] = useState({
+        state: "initial",
+    });
 
     const searchBook = async () => {
         try {
             const response = await axios(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${APIKey}&maxResults=40`);
             console.log(response.data.items)
             setBooks(response.data.items);
+            setFetchState({
+                state: "success",
+            });
+            console.log(fetchState)
 
         } catch (error) {
             console.log(error);
+            setFetchState({
+                state: "error",
+            })
         }
     }
-    return { books, search, setSearch, searchBook };
+    return { books, search, setSearch, searchBook, fetchState };
 };
