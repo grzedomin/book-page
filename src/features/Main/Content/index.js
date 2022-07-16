d5317040ea7e691c37ec119da1c276f3610d379a
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-16 23:07:50 +0200

    case initial deleted, commented code deleted

--- src/features/Main/Content/index.js
+++ src/features/Main/Content/index.js
@@ -1,48 +1,14 @@
 import ErrorBox from "./ErrorBox";
 import { BooksContainer } from "./Books";
 
-
 export const Content = ({ books, status }) => {
 
     switch (status.state) {
-        case "initial":
-            return null;
-
         case "error":
             return <ErrorBox />;
-
         case "success":
             return <BooksContainer books={books} />;
-
         default: 
             return null;
-    }
+    };
 };
-
-
-
-/*
-
-<Wrapper>
-            {books.map((book) => {
-
-                const thumbnail = book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail;
-                const title = book.volumeInfo.title;
-                const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(<br />) : "author undefined";
-                const categories = book.volumeInfo.categories ? book.volumeInfo.categories : "No category";
-
-                return (
-                    <Tile key={book.id}>
-                        <BookImg src={thumbnail} alt=""></BookImg>
-                        <Info>
-                            <Title>{title}</Title>
-                            <Author>{authors}</Author>
-                            <Category>{categories}</Category>
-                        </Info>
-                    </Tile>
-                )
-            }
-            )}
-        </Wrapper>
-
-*/
